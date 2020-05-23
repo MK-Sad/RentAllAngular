666ed5e1c0b1b3b967cc5ea7414bc94cdc13dd83
MonikaSadłowska <monikasadlowska@postgraduate.put.poznan.pl>
2020-05-23 10:13:27 +0200

    fix

--- src/app/logging/logging.component.ts
+++ src/app/logging/logging.component.ts
@@ -16,7 +16,7 @@ export class LoggingComponent implements OnInit {
   loggedUserName: string;
   userPoints: number;
   userCredentials: UserCredentials;
-  background_graty: string = this.shareService.homeUrl + '/images/graty.jpg';
+  background_graty: string = this.shareService.homeUrl + '/images/background.jpg';
   logo: string = this.shareService.homeUrl + '/images/logo.png';
 
   changeUserName(): void {
