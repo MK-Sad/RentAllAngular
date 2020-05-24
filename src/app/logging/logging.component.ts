e5200d5347a55d84651438e2dc67fec221b25702
Piotr Osiwianski <piotr.osiwianski@yahoo.com>
2020-05-24 11:26:08 +0200

    Add registration

--- src/app/logging/logging.component.ts
+++ src/app/logging/logging.component.ts
@@ -3,28 +3,32 @@ import { ShareService } from '../share.service';
 import { UserCredentials } from '../userCredentials';
 import { UserService } from '../user.service';
 import { NgForm } from '@angular/forms';
+import { UserPoints } from './../userPoints';
+import { User } from '../user';
 
 @Component({
   selector: 'app-logging',
   templateUrl: './logging.component.html',
-  styleUrls: ['./logging.component.css']
+  styleUrls: ['./logging.component.css'],
 })
 export class LoggingComponent implements OnInit {
-
-  constructor(private userService: UserService, private shareService: ShareService) {}
+  constructor(
+    private userService: UserService,
+    private shareService: ShareService
+  ) {}
 
   loggedUserName: string;
   userPoints: number;
   userCredentials: UserCredentials;
   background_graty: string = this.shareService.homeUrl + '/images/graty.jpg';
   logo: string = this.shareService.homeUrl + '/images/logo.png';
+  registerPopup: boolean = false;
 
   changeUserName(): void {
     this.shareService.userNameChange(this.loggedUserName);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   logout() {
     this.loggedUserName = null;
@@ -32,16 +36,35 @@ export class LoggingComponent implements OnInit {
   }
 
   login(formData: NgForm) {
-    const userCredentials: UserCredentials = { "name": formData.value.name, "password": formData.value.password };
-    this.userService
-      .authenticate(userCredentials)
-      .subscribe(userPoints => {
-        this.loggedUserName = userPoints.name;
-        this.userPoints = userPoints.points;
-        this.changeUserName();
-      });
+    const userCredentials: UserCredentials = {
+      name: formData.value.name,
+      password: formData.value.password,
+    };
+    this.userService.authenticate(userCredentials).subscribe((userPoints) => {
+      this.loggedUserName = userPoints.name;
+      this.userPoints = userPoints.points;
+      this.changeUserName();
+    });
   }
-
+  openRegister() {
+    this.registerPopup = true;
+  }
+  backToLogin(): void {
+    this.registerPopup = false;
+  }
+  register(formData: NgForm): void {
+    const user: User = {
+      name: formData.value.name,
+      password: formData.value.password,
+      eMail: formData.value.eMail,
+      phoneNumber: formData.value.phoneNumber,
+      points: 0,
+    };
+    this.userService.updateUser(user).subscribe((x) => {
+      this.registerPopup = false;
+      console.log(x);
+    });
+  }
 }
 
 /*Piotr
